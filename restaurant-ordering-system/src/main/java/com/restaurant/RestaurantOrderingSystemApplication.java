40b0f1ddf5495024312c14d14cca1c65e0e78c79
lewshubei <bei-02@graduate.utm.my>
2026-05-28 13:37:19 +0800

    environment setup for assignment 2

---
+++ restaurant-ordering-system/src/main/java/com/restaurant/RestaurantOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderingSystemApplication.class, args);
+	}
+
+}
